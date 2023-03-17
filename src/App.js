2c59fa6620fadff25799f1273ef0ad76e2de37be
MasudRana15924 <masudrana15924@outlook.com>
2023-03-17 21:01:17 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-import Home from './components/Home';
 import UserSignup from './pages/user/UserSignup';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/user-signup" element={<UserSignup/>} />
-      </Routes>
-    </BrowserRouter>
+
+    <Routes>
+      <Route path="/user-signup" element={<UserSignup />} />
+    </Routes>
+
   );
 }
 export default App;

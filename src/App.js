31fcf66e5c9867c6dea46aea5a7664d22206e094
MasudRana15924 <masudrana15924@outlook.com>
2023-03-18 21:36:59 +0600

    done

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {
 import UserSignup from './pages/user/UserSignup';
 import Login from './pages/user/Login';
 import ConfirmEmail from './pages/user/ConfirmEmail';
+import Changepassword from './pages/user/Changepassword';
 
 
 function App() {
   return (
      <Routes>
       <Route path="/user/email/confirm" element={<ConfirmEmail/>}></Route>
+      <Route path="/user/password" element={<Changepassword/>}></Route>
      </Routes>
   );
 }

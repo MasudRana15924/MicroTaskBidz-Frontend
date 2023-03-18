65af58a6402c4d9048649102d370aaccfbc072c1
MasudRana15924 <masudrana15924@outlook.com>
2023-03-18 20:30:04 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -11,11 +10,7 @@ import Login from './pages/user/Login';
 function App() {
   return (
    
-      <Routes>
-        <Route path="/user-signin" element={<Login/>} />
-
-        <Route path="/user-signup" element={<UserSignup/>} />
-      </Routes>
+    <></>
     
   );
 }

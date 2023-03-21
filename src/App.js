3355b81a4b01944d94171efd2f526c249a023c96
MasudRana15924 <masudrana15924@outlook.com>
2023-03-21 23:59:36 +0600

    Routes added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {
   Route,
 } from "react-router-dom";
 import Login from './pages/user/Login';
+import UserSignup from './pages/user/UserSignup';
 
 function App() {
   return (
     <Routes >
       <Route path="/user-signin"  element={< Login/>}></Route>
+      <Route path="/user-signup"  element={< UserSignup/>}></Route>
     </Routes>
   );
 }

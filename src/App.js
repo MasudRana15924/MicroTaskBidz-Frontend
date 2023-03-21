239ef9a10c9eed828cc8cb58c4113c7f30389f6a
MasudRana15924 <masudrana15924@outlook.com>
2023-03-22 00:22:36 +0600

    change password page added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import UserSignup from './pages/user/UserSignup';
-import Login from './pages/user/Login';
-import ConfirmEmail from './pages/user/ConfirmEmail';
 import Changepassword from './pages/user/Changepassword';
 
 
 function App() {
   return (
      <Routes>
-      <Route path="/user/email/confirm" element={<ConfirmEmail/>}></Route>
-      <Route path="/user/password" element={<Changepassword/>}></Route>
+      <Route path="/user/changepassword" element={<Changepassword/>}></Route>
      </Routes>
   );
 }

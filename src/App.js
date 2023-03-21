93a3e6b434246826814e91df6d6fc25f45a72951
MasudRana15924 <masudrana15924@outlook.com>
2023-03-22 00:53:32 +0600

    Header & footer designed

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import UserSignup from './pages/user/UserSignup';
-import Login from './pages/user/Login';
-import ConfirmEmail from './pages/user/ConfirmEmail';
-
-import UpdateInfo from './pages/user/UpdateInfo';
-
-
 
 
 function App() {
   return (
-     <Routes>
-      <Route path="/user" element={<UpdateInfo/>}></Route>
+     <>
 
-  
-     </Routes>
+     </>
   );
 }
 export default App;
--- src/pages/shared/Footer.js
+++ src/pages/shared/Footer.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import {ImFacebook } from "react-icons/im";
-import {ImTwitter } from "react-icons/im";
-import {ImLinkedin2 } from "react-icons/im";
-import {ImYoutube} from "react-icons/im";
+import { ImFacebook } from "react-icons/im";
+import { ImTwitter } from "react-icons/im";
+import { ImLinkedin2 } from "react-icons/im";
+import { ImYoutube } from "react-icons/im";
 
 const FOoter = () => {
   return (
-    <div className="p-10 bg-black">
+    <div className="p-10 bg-black mt-5 ">
       <div className="max-w-full mx-auto ">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:col-span-2 gap-2 ">
           <div className="lg:w-8/12 lg:mx-auto text-end  mb-5">
-          <h2 className="text-start text-white  font-bold">DROP US A LINE</h2>
+            <h2 className="text-start text-white  font-bold">DROP US A LINE</h2>
             <h2 className="text-start text-white text-2xl mt-3 font-bold">WE"RE MORE TALKATIVE</h2>
             <h2 className="text-start text-white text-2xl mb-5 font-bold">ALIVE 24HOUR IN A WEEK</h2>
             <form action="" className='mt-5 '>
-              <input type="text" placeholder='Name         |' />
-              <input type="text" placeholder='Email          |' />
-              <input type="text" placeholder='Phone        |' />
-              <input type="text" placeholder='Message    |' />
-              <button className="lg:w-3/12 bg-teal-700">+ Submit</button>
+              <div>
+                <input type="text" placeholder='Name         |' className="lg:w-full mb-3 h-10 border-gray-100 px-5" /> <br />
+                <input type="text" placeholder='Email          |' className="lg:w-full mb-3 h-10 border-gray-100 px-5" /> <br />
+                <input type="text" placeholder='Phone        |' className="lg:w-full mb-3 h-10 border-gray-100 px-5" /> <br />
+                <input type="text" placeholder='Message    |' className="lg:w-full mb-3 h-10 border-gray-100 px-5" />
+              </div>
+              <div>
+                <button className="bg-rose-700 mt-5 h-10 w-1/5 text-white font-semibold text-center mb-5">+ Submit</button>
+              </div>
             </form>
           </div>
           <div className="">
@@ -40,10 +44,10 @@ const FOoter = () => {
               <h2 className="text-start text-white font-semibold mt-1">Phone (123) 456 789</h2>
               <h2 className="text-start text-white font-semibold mt-1 mb-5">Email : help@raindesign.com</h2>
               <div className="flex items-start justify-start mb-5">
-              <ImFacebook className="bg-teal-700 text-white text-4xl rounded-full text-center"></ImFacebook>
-              <ImTwitter className="bg-zinc-600 text-white text-4xl rounded-full text-center mx-5 p-2"></ImTwitter>
-              <ImLinkedin2  className="bg-zinc-600 text-white text-4xl rounded-full text-center  p-2"></ImLinkedin2>
-              <ImYoutube className="bg-zinc-600 text-white text-4xl rounded-full text-center mx-3  p-2"></ImYoutube>
+                <ImFacebook className="bg-teal-700 text-white text-4xl rounded-full text-center"></ImFacebook>
+                <ImTwitter className="bg-zinc-600 text-white text-4xl rounded-full text-center mx-5 p-2"></ImTwitter>
+                <ImLinkedin2 className="bg-zinc-600 text-white text-4xl rounded-full text-center  p-2"></ImLinkedin2>
+                <ImYoutube className="bg-zinc-600 text-white text-4xl rounded-full text-center mx-3  p-2"></ImYoutube>
               </div>
               <h2 className="text-white text-start text-sm"> &copy; 2021 Rain Design .All rights reserved</h2>
               <h2 className="text-white text-start text-sm"> Made by Rain Group of Company</h2>

a35a20b58e453db446d1a915137ae67e108b5b53
MasudRana15924 <masudrana15924@outlook.com>
2023-03-22 00:36:06 +0600

    User Dashboard designed

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-
 import UserDetails from './pages/user/UserDetails';
-
-
-
 
 function App() {
   return (
-
-    <>
     <Routes >
-
       <Route path="/user-info"  element={< UserDetails/>}></Route>
     </Routes>
-    </>
-
-
   );
 }
 export default App;

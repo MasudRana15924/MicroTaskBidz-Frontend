a14540ce14ee1ed9d2c633681f4e1d5410b76cb6
MasudRana15924 <masudrana15924@outlook.com>
2023-03-22 00:26:37 +0600

    routes added in app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-
 import ResetPassword from './pages/user/Resetpassword';
 import Login from './pages/user/Login';
 import UserSignup from './pages/user/UserSignup';
@@ -16,7 +15,7 @@ function App() {
       <Route path="/user-signin"  element={< Login/>}></Route>
       <Route path="/user-signup"  element={< UserSignup/>}></Route>
       <Route path="/user/password" element={<ResetPassword/>}></Route>
-          <Route path="/user/changepassword" element={<Changepassword/>}></Route>
+      <Route path="/user/changepassword" element={<Changepassword/>}></Route>
     </Routes>
 
   );

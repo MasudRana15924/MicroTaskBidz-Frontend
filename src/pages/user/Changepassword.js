c9fe5894a85901ab9bdd73b5b8bc8619427a6a0f
MasudRana15924 <masudrana15924@outlook.com>
2023-03-24 22:30:40 +0600

    error fixed

--- src/pages/user/Changepassword.js
+++ src/pages/user/Changepassword.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import UserSidebar from './UserSidebar';
 
 
 
@@ -22,35 +23,7 @@ const Changepassword = () => {
         [e.target.name]: e.target.value
       });
     };
-    return (
-        <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <div class="w-full max-w-md">
-          <div class="bg-white rounded-lg shadow-lg p-8">
-            <h2 class="mt-5 mb-5 text-2xl font-medium  text-start">Change Passowrd</h2>
-            <form onSubmit={registerSubmit}>
-              <div class="mb-4">
-                <input class="border border-gray-200 w-full h-10 rounded p-3" type="text" id="username"
-                  name="name"
-                  value={oldpassword}
-                  onChange={registerDataChange}
-                  placeholder="Enter Your 6 digit code"
-                  required
-                />
-              </div>
-             
-              <div class="mb-4">
-  
-                <input class="border border-gray-200 w-full h-10 rounded p-3" type="password" id="userpassword"
-                  name="password"
-                  value={newpassword}
-                  onChange={registerDataChange}
-                  placeholder="Enter Your Password"
-                  required
-                />
-              </div>
-              <button class="border-2 border-teal-700 bg-teal-700 text-white py-1 w-full rounded-md  font-semibold h-10 bg-teal-700 mt-5" type="submit">Change Password</button>
-              
-            </form>
+    
 
   return (
     <div className="p-10 bg-white mt-20 mb-20">

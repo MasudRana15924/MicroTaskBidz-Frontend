4cb88d420461093867e536113f988c63b0642c70
MasudRana15924 <masudrana15924@outlook.com>
2023-03-24 22:16:59 +0600

    login page edited

--- src/pages/user/Changepassword.js
+++ src/pages/user/Changepassword.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+
 
 const Changepassword = () => {
-    const dispatch = useDispatch();
     const [user, setUser] = useState({
       oldpassword: "",
       newpassword: "",
@@ -26,7 +25,7 @@ const Changepassword = () => {
         <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <div class="w-full max-w-md">
           <div class="bg-white rounded-lg shadow-lg p-8">
-            <h2 class="mt-5 mb-5 text-2xl font-medium mb-4 text-start">Change Passowrd</h2>
+            <h2 class="mt-5 mb-5 text-2xl font-medium  text-start">Change Passowrd</h2>
             <form onSubmit={registerSubmit}>
               <div class="mb-4">
                 <input class="border border-gray-200 w-full h-10 rounded p-3" type="text" id="username"

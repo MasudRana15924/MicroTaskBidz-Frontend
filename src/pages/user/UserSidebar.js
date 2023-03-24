bd2e9168f9769f14572d2745cb60637cd52b2d02
MasudRana15924 <masudrana15924@outlook.com>
2023-03-24 21:21:43 +0600

    sidebar responsive

--- src/pages/user/UserSidebar.js
+++ src/pages/user/UserSidebar.js
@@ -8,14 +8,14 @@ import { Link } from 'react-router-dom';
 const UserSidebar = () => {
     return (
         <div className="">
-            <div className=" flex items-center  mx-5 border border-gray-50 w-3/4 shadow-xl mb-10">
+            <div className="w-full  flex items-center border border-gray-50  lg:mx-5 lg:w-3/4 shadow-xl mb-10">
                 <img src={userImg} alt="" className="rounded-full h-20" />
                 <div className="text-start mx-5 ">
                     <h2>Hello,</h2>
                     <h2 className="text-xl font-semibold">Masud Rana</h2>
                 </div>
             </div>
-            <div className="mx-5 border border-gray-50 w-3/4 shadow-xl mb-3 mt-5 text-start">
+            <div className=" border border-gray-50 w-full shadow-xl mb-3 mt-5 text-start lg:w-3/4 lg:mx-5 ">
                 <div className="flex items-center mt-20">
                     <AiOutlineUnorderedList className="mt-1 w-20 h-5"></AiOutlineUnorderedList>
                     <h2 className="text-sm lg:text-xl  lg:font-semibold"> Manage My Accounts </h2>

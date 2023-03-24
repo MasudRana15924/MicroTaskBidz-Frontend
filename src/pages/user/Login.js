cf500e07d2c8dcaf3682176a0aa350b81e20e691
MasudRana15924 <masudrana15924@outlook.com>
2023-03-24 18:50:07 +0600

    token generate

--- src/pages/user/Login.js
+++ src/pages/user/Login.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './User.css'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createLogin } from '../../state/Login/loginSlice';
+
 
 const Login = () => {
     const dispatch = useDispatch();
+    const { token } = useSelector(
+        (state) => state.userDetails.user
+    );
+
     const [user, setUser] = useState({
 
         email: "",
@@ -40,16 +45,16 @@ const Login = () => {
                         <button className="py-3 px-6 rounded-xl bg-rose-700 hover:bg-blue-100 focus:bg-blue-100 active:bg-blue-200">
                             <div className="flex gap-4 justify-center">
                                 <Link to="/user-signin">
-                                <span className="block w-max font-medium tracking-wide text-md text-center text-white">User</span>
+                                    <span className="block w-max font-medium tracking-wide text-md text-center text-white">User</span>
                                 </Link>
                             </div>
                         </button>
                         <button className="py-3 px-6 rounded-xl bg-gray-900 transition hover:bg-gray-800 active:bg-gray-600 focus:bg-gray-700">
                             <div className="flex gap-4 items-center justify-center text-white">
 
-                               <Link to="/contractor-signin">
-                               <span className="block w-max font-medium tracking-wide text-sm text-white">Contractor</span>
-                               </Link>
+                                <Link to="/contractor-signin">
+                                    <span className="block w-max font-medium tracking-wide text-sm text-white">Contractor</span>
+                                </Link>
                             </div>
                         </button>
                     </div>
@@ -92,10 +97,13 @@ const Login = () => {
                         </div>
 
                         <div>
-                            <button className="w-full px-6 py-3 rounded-xl bg-teal-700 mb-5">
-                                <span className="font-semibold text-white text-lg">Login</span>
-                            </button>
-
+                            {
+                                token ? <button className="w-full px-6 py-3 rounded-xl bg-teal-700 mb-5">
+                                    <span className="font-semibold text-white text-lg">Logout</span>
+                                </button> : <button className="w-full px-6 py-3 rounded-xl bg-teal-700 mb-5">
+                                    <span className="font-semibold text-white text-lg">Login</span>
+                                </button>
+                            }
                             <span className="text-sm tracking-wide text-gray-400 mt-5">Don't have any account ?</span> <Link to="/user-signup"> <span className="text-blue-600">Create new account</span>
                             </Link>
                         </div>
